5e3fe8eaf2327d708499a95e8c0b7f3ff541ada9
inshad2005 <promatics.inshad@gmail.com>
2017-12-02 18:57:37 +0530

    222565

---
+++ src/style/include/page-header.php
@@ -0,0 +1,231 @@
+<header class="header-sec page-header">
+    <div class="top-header">
+        <div class="wrap-pageh">
+           <div class="col-md-3 col-sm-3 col-xs-12">
+                <div class="logo-top">
+                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" class="img-responsive"></a>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="header-search-bar">
+                    <form class="horizontal-form customize">
+                        <div class="col-md-12 col-sm-12 p-0">
+                            <div class="col-md-5 col-sm-5 col-xs-12 no-padding">
+                                <input  type="text" class="form-control input1" name="password" placeholder="Find a service or saloon">
+                            </div>
+                            <div class="col-md-5 col-sm-5 col-xs-12 no-padding">
+                                <input type="text" class="form-control input2"  placeholder="Enter your area">
+                            </div>
+                            <div class="col-md-2 col-sm-2 col-xs-12 no-padding">
+                                <a class="btn custom-btn" href="javascript:void(0);">Search</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <div class="pull-right left-navbar">
+                   <ul class="right-login">
+                        <li><a href="#" data-toggle="modal" data-target="#signupAs">Sign Up</a></li>
+                        <li><a href="login.php">Login</a></li>
+                   </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->  
+        <div class="bottom-header">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav cus-navbar custome-bar">
+                        <li class="dropdown">
+                            <a href="#">Hair</a>
+                            <ul class="sub-service dropdown-content">
+                                <li>
+                                    <a href="javascript:void(0);">Hair Coloring</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Women's Haircut</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Men's Haircut</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Hair Trimming</a>
+                                </li>
+                                <li class="see-all">
+                                        <a href="javascript:void(0);">View Hair Treatment
+                                         <span class="icon-right-li">
+                                            <i class="fa fa-angle-right"></i>        
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#">Nails</a>
+                            <ul class="sub-service dropdown-content">
+                                <li>
+                                    <a href="javascript:void(0);">Manicure</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Pedicure</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Gel Nails</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Acrylic Nails</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">French Manicure</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">French Pedicure</a>
+                                </li>
+                                <li class="see-all">
+                                        <a href="javascript:void(0);">View Nail Treatment
+                                         <span class="icon-right-li">
+                                            <i class="fa fa-angle-right"></i>        
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                       <li class="dropdown">
+                            <a href="#">Massage</a>
+                            <ul class="sub-service dropdown-content">
+                                <li>
+                                    <a href="javascript:void(0);">Bali Massage</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Relaxing Massage</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Full Body Massage</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Shoulder Massage</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Thai Massage</a>
+                                </li>
+                                <li class="see-all">
+                                        <a href="javascript:void(0);">View Massage Services
+                                         <span class="icon-right-li">
+                                            <i class="fa fa-angle-right"></i>        
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                       <li class="dropdown">
+                            <a href="#">Spa</a>
+                            <ul class="sub-service dropdown-content">
+                                <li>
+                                    <a href="javascript:void(0);">Hammam</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Body Scrub</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Cellulite Treatments</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Body Wrap</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Slimming Treatments</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Foot Scrub</a>
+                                </li>
+                                <li class="see-all">
+                                        <a href="javascript:void(0);">View spa Treatment
+                                         <span class="icon-right-li">
+                                            <i class="fa fa-angle-right"></i>        
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                       <li class="dropdown">
+                            <a href="#">barbers</a>
+                            <ul class="sub-service dropdown-content">
+                                <li>
+                                    <a href="javascript:void(0);">Men Shave</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Beard trimming</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Clipper Cut</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);">Hot Towel Shave</a>
+                                </li>
+                                <li class="see-all">
+                                        <a href="javascript:void(0);">View Barber Services
+                                         <span class="icon-right-li">
+                                            <i class="fa fa-angle-right"></i>        
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /.navbar-collapse -->
+    </nav>
+</header>
+<!-- End Header -->
+<!-- Modal -->
+<div class="modal fade signup" id="signupAs" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+            <h4 class="modal-title" id="myModalLabel">Sign Up As</h4>
+        </div>
+        <div class="modal-body">
+            <div class="sign-selectoption">
+                <div class="col-md-12 col-xs-12 col-sm-12 p-0">
+                    <div class="col-md-6 col-xs-12  col-sm-6 right-border">
+                        <div class="customer-img">
+                            <img src="img/saloon.svg" class="img-responsive">
+                        </div>
+                        <a  class="cus-btn btn" href="saloon-signup.php">Saloon</a>
+                    </div>
+                    <div class="col-md-6 col-xs-12  col-sm-6">
+                        <div class="professional-img">
+                            <div class="customer-img">
+                                <img src="img/professional.svg" class="img-responsive">
+                            </div>
+                        </div>
+                        <a class="cus-btn btn" href="customer-signup.php">Customer</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+<!--  <script type="text/javascript">
+    $('.custome-bar > li.dropdown').hover(function(){
+       $(this).addClass('active').siblings('li.dropdown').removeClass('active');;
+    });
+</script> -->
