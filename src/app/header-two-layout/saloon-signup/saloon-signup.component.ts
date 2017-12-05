8d1db320113e05070191c9e29795bb0a2e16b9f2
inshad2005 <promatics.inshad@gmail.com>
2017-12-05 12:03:29 +0530

    12/05/17

--- src/app/header-two-layout/saloon-signup/saloon-signup.component.ts
+++ src/app/header-two-layout/saloon-signup/saloon-signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 declare var $
 @Component({
     selector: 'app-saloon-signup',
@@ -7,7 +8,13 @@ declare var $
     styleUrls: ['./saloon-signup.component.scss']
 })
 export class SaloonSignupComponent implements OnInit {
-    constructor(public router: Router) {}
+	 reactiveForm: FormGroup;
+    constructor(public router: Router, private fb: FormBuilder,) {
+       this.reactiveForm = fb.group({
+            'loginAs': [null, Validators.compose([Validators.required])],
+            
+        }) 
+    }
 
     ngOnInit() {
 
--- src/app/header-two-layout/saloon-signup/saloon-signup.module.ts
+++ src/app/header-two-layout/saloon-signup/saloon-signup.module.ts
@@ -1,11 +1,12 @@
 import { NgModule,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SaloonSignupRoutingModule } from './saloon-signup-routing.module';
 import { SaloonSignupComponent } from './saloon-signup.component';
 
 @NgModule({
-    imports: [CommonModule, SaloonSignupRoutingModule],
+    imports: [CommonModule, SaloonSignupRoutingModule,ReactiveFormsModule],
     declarations: [SaloonSignupComponent]
 })
 export class SaloonSignupModule implements OnInit  {
