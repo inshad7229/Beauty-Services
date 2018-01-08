1d4db01049892778886cf0bd1f40792eb6ba3ca0
inshad7229 <ins7229@gmail.com>
2018-01-08 19:41:20 +0530

    jjjjhn

--- src/app/header-two-layout/searched-saloon/searched-saloon.component.ts
+++ src/app/header-two-layout/searched-saloon/searched-saloon.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastsManager , Toast} from 'ng2-toastr';
+import { forkJoin } from "rxjs/observable/forkJoin";
 
 import {SaloonService} from '../../providers/saloon.service'
 import {SaloonDetailsModel,AccountCreationModel,VerifiactionModel} from '../../models/saloon.modal';
@@ -20,17 +21,40 @@ export class SearchedSaloonComponent implements OnInit {
 	verifiactionModel:VerifiactionModel=new VerifiactionModel();
 	waitLoader
    saloonList
+   serviceList=[]
+   afterFilterSaloon=[]
+   saloonListBackup1
+   saloonListBackup2
+   autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = false;
+  filter
+
+   get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(v) {
+    this._tickInterval = Number(v);
+  }
+  private _tickInterval = 1;
     constructor(public router: Router, 
     	        private saloonServices:SaloonService,
     	        vcr: ViewContainerRef,
     	        private toastr: ToastsManager,
     	        private commonService:CommonService,
     	        private appProvider:AppProvider) {
+              this.filter={}
     	        this.toastr.setRootViewContainerRef(vcr);
     }
 
     ngOnInit() {
-           //alert(this.appProvider.current.serviceSearched)
 				$(window).scroll(function() {
 			        if ($(this).scrollTop() > 1){  
 			            $('header').addClass("sticky");
@@ -39,111 +63,27 @@ export class SearchedSaloonComponent implements OnInit {
 			            $('header').removeClass("sticky");
 			        }
 			    });
-		// 	var wow = new WOW(
-		// 	        {
-		// 	            animateClass: 'animated',
-		// 	            offset:       100,
-		// 	            callback:     function(box) {
-		// 	                console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-		// 	            }
-		// 	        }
-		// 	    );
-		// wow.init();
-		// 	$(window).load(function() {
-		// 	        $(".datetimepicker1");
-		// 	    });
-		// 	$('.datetimepicker1').datetimepicker({
-		// 	        //lang:'ch',
-		// 	         //yearOffset:200,       
-		// 	        timepicker:false,
-		// 	        format:'d-m-Y',
-		// 	        formatDate:'d-m-Y',
-		// 	        value:'Enter Date',
-		// 	        step:10,
-		// 	        minDate:'+1d',
-		// 	        scrollMonth:false,
-		// 	        scrollTime:false,
-		// 	        scrollInput:true,
-		// 	    });
-		// 	var mySlider = $("#rane-slide").slider({
-		// 	        'tooltip':'hide'
-		// 	    });
-		// 	    // Cal l a method on the slider
-		// 	    mySlider.on('change',function(){
-		// 	        var value = mySlider.slider('getValue');
-		// 	        $('.min-range').html('<b>$'+value[0]+'</b>');
-		// 	        $('.max-range').html('<b>$'+value[1]+'</b>');
-		// 	    });
 			    this.getserviceList()
 		}
 
-		onContinue(){
-	     this.saloonServices.SaloonSignup(this.saloonDetailsModel)
-        .subscribe((data)=>{
-            console.log(data);
-            if(data.response){
-              this.toastr.success( data.message ,'Authentication',{toastLife: 3000, showCloseButton: true})
-              var wow = new WOW(
-              {
-                  animateClass: 'animated',
-                  offset:       100,
-                  callback:     function(box) {
-                      console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-                  }
-              }
-          );
-    wow.init();
-      $(window).load(function() {
-              $(".datetimepicker1");
-          });
-      $('.datetimepicker1').datetimepicker({
-              //lang:'ch',
-               //yearOffset:200,       
-              timepicker:false,
-              format:'d-m-Y',
-              formatDate:'d-m-Y',
-              value:'Enter Date',
-              step:10,
-              minDate:'+1d',
-              scrollMonth:false,
-              scrollTime:false,
-              scrollInput:true,
-          });
-      var mySlider = $("#rane-slide").slider({
-              'tooltip':'hide'
-          });
-          // Cal l a method on the slider
-          mySlider.on('change',function(){
-              var value = mySlider.slider('getValue');
-              $('.min-range').html('<b>$'+value[0]+'</b>');
-              $('.max-range').html('<b>$'+value[1]+'</b>');
-          });
-              // setTimeout(()=>{
-				          //  //this.router.navigate(['/login']);
-              // },3000)
-            //	alert(data.message)
-            }else if (data.message=='Email not found in database') {
-               this.toastr.error('Please insert your register Email' ,'Authentication Failed ',{toastLife: 3000, showCloseButton: true});
-              // code...
-            }else {
-              this.toastr.error( 'Something Went Wrong Please Try Again' ,'Authentication Failed ',{toastLife: 3000, showCloseButton: true});
-            }
-        })
-		}
 
 
 getserviceList(){
+          this.saloonList=[]
           this.waitLoader=true
         this.commonService.getAllSaloonList()
+         forkJoin([this.commonService.getAllSaloonList(),this.commonService.getServices()])
         .subscribe((data)=>{
             var list=[]
              this.waitLoader=false
             console.log(data);
-            if(data.response){
-                this.saloonList=data.data
-               
-              // this.toastr.success(data.message ,'Services Added successfully ',{toastLife: 1000, showCloseButton: true})
-              // this.router.navigate(['/header-three-layout/service-list']);
+            if(data[0].response){
+                this.saloonList=this.saloonList.concat(data[0].data.slice(0))
+                this.saloonListBackup1=data[0].data.slice(0)
+                this.saloonListBackup2=data[0].data.slice(0)
+            }
+            if(data[1].response){
+                this.serviceList=data[1].data
             }
          }) 
     }
@@ -209,4 +149,246 @@ getClass(name){
    	return 'relevant-de'
    }
   }
+
+      getRatingClass(rating,flag){
+      let count:number=0;
+      let avg:number=0
+      for (var i = 0; i < rating.length; ++i) {
+        // code...rating
+        if (rating[i].rating) {
+          // code...
+          count=count+parseInt(rating[i].rating)
+        }
+      }
+       avg=count/rating.length
+      if (flag==1) {
+        if (avg<1) {
+          return 'fa fa-star-half-o'
+        }else{
+          return 'fa fa-star'
+        }
+      }
+     else if (flag==2) {
+        if (avg<2) {
+          return 'fa fa-star-half-o'
+        }else{
+          return 'fa fa-star'
+        }
+      }
+
+      else if (flag==3) {
+        if (avg<3) {
+          return 'fa fa-star-half-o'
+        }else{
+          return 'fa fa-star'
+        }
+      }
+     else if (flag==4) {
+        if (avg<4) {
+          return 'fa fa-star-half-o'
+        }else{
+          return 'fa fa-star'
+        }
+      }
+    else  if (flag==5) {
+        if (avg<5) {
+          return 'fa fa-star-half-o'
+        }else{
+          return 'fa fa-star'
+        }
+      }
+    }
+
+
+    findRangclass(rang ,flag){
+       if (flag==1) {
+          if (parseInt(rang)>=flag) {
+            return 'active'
+          }
+       }else if (flag==2) {
+          if (parseInt(rang)>=flag) {
+            return 'active'
+          }
+       }else if (flag==3) {
+          if (parseInt(rang)>=flag) {
+            return 'active'
+          }
+       }else if (flag==4) {
+          if (parseInt(rang)>=flag) {
+            return 'active'
+          }
+       }
+    }
+
+    unique(array){
+         return array.filter(function(el, index, arr) {
+                  return index == arr.indexOf(el);     
+              }); 
+    }
+
+  onFirst(status){
+    if (status==true) {
+     let data=this.saloonListBackup1.filter(arg=>arg.price_range=='1')
+     this.afterFilterSaloon=this.afterFilterSaloon.concat(data)
+     this.saloonList=this.unique(this.afterFilterSaloon)
+     this.saloonList=this.unique(this.saloonList)
+     if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }else{
+      this.saloonList=this.saloonList.filter(arg=>arg.price_range!='1')
+      this.afterFilterSaloon=this.afterFilterSaloon.filter(arg=>arg.price_range!='1')
+      if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }
+
+  }
+  onSecond(status){
+     if (status==true) {
+     let data=this.saloonListBackup1.filter(arg=>arg.price_range=='2')
+      this.afterFilterSaloon=this.afterFilterSaloon.concat(data)
+     this.saloonList=this.unique(this.afterFilterSaloon)
+     this.saloonList=this.unique(this.saloonList)
+     if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }else{
+      this.saloonList=this.saloonList.filter(arg=>arg.price_range!='2')
+      this.afterFilterSaloon=this.afterFilterSaloon.filter(arg=>arg.price_range!='2')
+      if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }
+  }
+  onThird(status){
+    if (status==true) {
+     let data=this.saloonListBackup1.filter(arg=>arg.price_range=='3')
+      this.afterFilterSaloon=this.afterFilterSaloon.concat(data)
+     this.saloonList=this.unique(this.afterFilterSaloon)
+     this.saloonList=this.unique(this.saloonList)
+     if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }else{
+      this.saloonList=this.saloonList.filter(arg=>arg.price_range!='3')
+      this.afterFilterSaloon=this.afterFilterSaloon.filter(arg=>arg.price_range!='3')
+      if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }
+  }
+  onFourth(status){
+    if (status==true) {
+     let data=this.saloonListBackup1.filter(arg=>arg.price_range=='4')
+      this.afterFilterSaloon=this.afterFilterSaloon.concat(data)
+     this.saloonList=this.unique(this.afterFilterSaloon)
+     this.saloonList=this.unique(this.saloonList)
+     if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }else{
+      this.saloonList=this.saloonList.filter(arg=>arg.price_range!='4')
+      this.afterFilterSaloon=this.afterFilterSaloon.filter(arg=>arg.price_range!='4')
+      if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }
+  }
+
+  onService(checkStatus,id){
+    if (checkStatus==true) {
+     let data=this.saloonListBackup1.filter(arg=>this.checkIndex(arg,id)==true)
+      this.afterFilterSaloon=this.afterFilterSaloon.concat(data)
+     this.saloonList=this.unique(this.afterFilterSaloon)
+     this.saloonList=this.unique(this.saloonList)
+     if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+    }else{
+      this.saloonList=this.saloonList.filter(arg=>this.checkIndex2(arg,id)==true)
+      this.afterFilterSaloon=this.afterFilterSaloon.filter(arg=>this.checkIndex2(arg,id)==true)
+      if (this.filter.sort) {
+       this.onSort(this.filter.sort)
+     }
+
+    } 
+  }
+// this.filterDist.map(function (img) { return img.name; }).indexOf(this.selectedDist[i])==-1)
+  checkIndex(arg,id):boolean{
+     if (arg.saloonServices.length>0) {
+       
+       if ( arg.saloonServices.map(function (img) { return +img.service_id; }).indexOf(id)!=-1) {
+         return true
+       }else{
+         return false
+       }
+     }else{
+       return false
+     }
+  }
+  checkIndex2(arg,id):boolean{
+     if (arg.saloonServices.length>0) {
+       
+       if ( arg.saloonServices.map(function (img) { return +img.service_id; }).indexOf(id)!=-1) {
+         return false
+       }else{
+         return true
+       }
+     }
+  }
+
+  onClearAll(){
+      this.filter.first=false
+      this.filter.two=false
+      this.filter.three=false
+      this.filter.four=false
+      for (var i = 0; i < this.serviceList.length; ++i) {
+        this.serviceList[i].checkStatus=false
+      }
+      this.saloonList=this.saloonListBackup2.slice(0)
+  }
+
+ onSort(value){
+ const data =this.saloonList.slice();
+    if (!value || value == '') {
+      this.saloonList = data;
+      
+      return;
+    }
+
+    this.saloonList.sort((a, b) => {
+      //alert(value)
+      let isAsc =  'asc';
+      switch (value) {
+
+        case 'low': return compare1(parseInt(a.price_range), parseInt(b.price_range), isAsc);
+        case 'high': return compare2(parseInt(a.price_range), parseInt(b.price_range), isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+function compare1(a, b, isAsc) {
+  return (a-b) 
+}
+
+function compare2(a, b, isAsc) {
+  return (b-a) 
+}
+
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    return dist
 }
--- src/app/header-two-layout/searched-saloon/searched-saloon.module.ts
+++ src/app/header-two-layout/searched-saloon/searched-saloon.module.ts
@@ -2,6 +2,8 @@ import { NgModule,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule }    from '@angular/common/http';
 import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import {MatSliderModule} from '@angular/material';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 
 import { SearchedSaloonRoutingModule } from './searched-saloon-routing.module';
 import { SearchedSaloonComponent } from './searched-saloon.component';
@@ -9,7 +11,13 @@ import {SaloonService} from '../../providers/saloon.service'
 import {CommonService} from '../../providers/common.service'
 
 @NgModule({
-    imports: [CommonModule, SearchedSaloonRoutingModule,HttpClientModule,ToastModule.forRoot()],
+    imports: [CommonModule,
+    SearchedSaloonRoutingModule,
+    HttpClientModule,MatSliderModule,
+    ToastModule.forRoot(),
+    ReactiveFormsModule,
+	FormsModule
+    ],
     declarations: [SearchedSaloonComponent],
     providers:[SaloonService,CommonService],
 
